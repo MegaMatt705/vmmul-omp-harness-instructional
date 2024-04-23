d0b2881731b988e7740e3cb3c9d656ad8341f1ac
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-23 16:13:00 -0700

    algorithms

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -5,26 +5,32 @@
 
 const char* dgemv_desc = "OpenMP dgemv.";
 
-/*
- * This routine performs a dgemv operation
- * Y :=  A * X + Y
- * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
- * On exit, A and X maintain their input values.
- */
-
 void my_dgemv(int n, double* A, double* x, double* y) {
+    // Temporary array to hold the result of A * X for each thread
+    double* temp_y = (double*)malloc(n * sizeof(double));
+    memset(temp_y, 0, n * sizeof(double)); // Initialize to zero
 
-   #pragma omp parallel
-   {
-      int nthreads = omp_get_num_threads();
-      int thread_id = omp_get_thread_num();
-      printf("my_dgemv(): Hello world: thread %d of %d checking in. \n", thread_id, nthreads);
-      printf("my_dgemv(): For actual timing runs, please comment out these printf() and omp_get_*() statements. \n");
-   }
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        int nthreads = omp_get_num_threads();
+        int chunk_size = n / nthreads;
+        int start = thread_id * chunk_size;
+        int end = (thread_id == nthreads - 1) ? n : start + chunk_size;
 
-   // insert your dgemv code here. you may need to create additional parallel regions,
-   // and you will want to comment out the above parallel code block that prints out
-   // nthreads and thread_id so as to not taint your timings
+        // Perform the matrix-vector multiplication for the portion of the matrix assigned to this thread
+        for (int i = start; i < end; i++) {
+            for (int j = 0; j < n; j++) {
+                temp_y[i] += A[i * n + j] * x[j];
+            }
+        }
+    }
 
+    // Combine the results from each thread
+    for (int i = 0; i < n; i++) {
+        y[i] += temp_y[i];
+    }
+
+    // Clean up
+    free(temp_y);
 }
-
--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,5 +1,7 @@
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
+#include <omp.h>
+
 /*
  * This routine performs a dgemv operation
  * Y :=  A * X + Y
@@ -7,6 +9,13 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-   // insert your code here: implementation of vectorized vector-matrix multiply
-
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
+        double sum = 0.0;
+        for (int j = 0; j < n; ++j) {
+            sum += A[i * n + j] * x[j];
+        }
+        y[i] += sum;
+    }
+}
 }
