20a2837d2f73c0f16a1f91f421cdc084a61e9c8d
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-29 16:55:25 -0700

    typo

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,7 +8,7 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv_omp_optimized(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double* A, double* x, double* y) {
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < n; ++i) {
         double sum = 0.0;
