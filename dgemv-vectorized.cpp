eb75103e98f2e15863d43d7dee17c57ff9c2438f
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-29 16:59:01 -0700

    loop unrolling

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,20 +8,19 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
-    const int blockSize = 64; // Choose a block size that fits into cache
-    for (int ii = 0; ii < n; ii += blockSize) {
-        for (int jj = 0; jj < n; jj += blockSize) {
-            for (int i = ii; i < ii + blockSize && i < n; ++i) {
-                double sum = 0.0;
-                for (int j = jj; j < jj + blockSize && j < n; ++j) {
-                    sum += A[i * n + j] * x[j];
-                }
-                y[i] += sum;
-            }
+void my_dgemv_unrolled(int n, double* A, double* x, double* y) {
+    for (int i = 0; i < n; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < n; j += 4) {
+            sum += A[i * n + j] * x[j];
+            sum += A[i * n + j + 1] * x[j + 1];
+            sum += A[i * n + j + 2] * x[j + 2];
+            sum += A[i * n + j + 3] * x[j + 3];
         }
+        y[i] += sum;
     }
 }
+
 
 
 
