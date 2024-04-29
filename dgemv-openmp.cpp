c6c1823d0e0e16af64eb85d3efec4e8cbde0a613
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-29 15:27:04 -0700

    comments

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -6,9 +6,9 @@
 const char* dgemv_desc = "OpenMP dgemv.";
 
 void my_dgemv(int n, double* A, double* x, double* y) {
-    // Temporary array to hold the result of A * X for each thread
+    // Temporary array to hold result of A * X for each thread
     double* temp_y = (double*)malloc(n * sizeof(double));
-    memset(temp_y, 0, n * sizeof(double)); // Initialize to zero
+    memset(temp_y, 0, n * sizeof(double));
 
     #pragma omp parallel
     {
@@ -18,7 +18,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
         int start = thread_id * chunk_size;
         int end = (thread_id == nthreads - 1) ? n : start + chunk_size;
 
-        // Perform the matrix-vector multiplication for the portion of the matrix assigned to this thread
+        // Matrix-vector multiplication
         for (int i = start; i < end; i++) {
             for (int j = 0; j < n; j++) {
                 temp_y[i] += A[i * n + j] * x[j];
@@ -26,11 +26,11 @@ void my_dgemv(int n, double* A, double* x, double* y) {
         }
     }
 
-    // Combine the results from each thread
+    // Basic corresponding element addition
     for (int i = 0; i < n; i++) {
         y[i] += temp_y[i];
     }
 
-    // Clean up
+    // Free the temp var
     free(temp_y);
 }
