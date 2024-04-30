980e27ce930c59bae7cc9f812df3f73621f65898
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-29 17:02:19 -0700

    trying initial again

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -9,18 +9,12 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * On exit, A and X maintain their input values.
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
-    for (int i = 0; i < n; i++) {
+    #pragma omp parallel for
+    for (int i = 0; i < n; ++i) {
         double sum = 0.0;
-        for (int j = 0; j < n; j += 4) {
+        for (int j = 0; j < n; ++j) {
             sum += A[i * n + j] * x[j];
-            sum += A[i * n + j + 1] * x[j + 1];
-            sum += A[i * n + j + 2] * x[j + 2];
-            sum += A[i * n + j + 3] * x[j + 3];
         }
         y[i] += sum;
     }
 }
-
-
-
-
