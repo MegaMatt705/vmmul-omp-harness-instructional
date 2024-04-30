1daf126eb5404203ef78a77e10bfaf1cd4c20037
Matthew Xavier Hernandez <77997996+MegaMatt705@users.noreply.github.com>
2024-04-29 17:55:50 -0700

    this assignment is the worst

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -9,16 +9,14 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv_blocked(int n, double* A, double* x, double* y, int blockSize) {
-    for (int ii = 0; ii < n; ii += blockSize) {
-        for (int jj = 0; jj < n; jj += blockSize) {
-            for (int i = ii; i < std::min(ii + blockSize, n); i++) {
-                double sum = 0.0;
-                for (int j = jj; j < std::min(jj + blockSize, n); j++) {
-                    sum += A[i * n + j] * x[j];
-                }
-                y[i] += sum;
-            }
+void my_dgemv_interchanged(int n, double* A, double* x, double* y) {
+    for (int j = 0; j < n; j++) {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += A[i * n + j] * x[j];
+        }
+        for (int i = 0; i < n; i++) {
+            y[i] += sum;
         }
     }
 }
